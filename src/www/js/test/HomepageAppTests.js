0a2ae9f0efc7324a0e2873ff6e7bd04bffa4aa1f
Leif Nelson <leif@lwndev.com>
2013-05-21 21:03:27 -0500

    Adding a second test

--- src/www/js/test/HomepageAppTests.js
+++ src/www/js/test/HomepageAppTests.js
@@ -2,3 +2,8 @@ test( "testMe test!", function() {
   ok( testMe("happy"),"testMe asserted successfully");
   notEqual( testMe("unhappy"), true, "testMe failed as expected");
 });
+
+test( "testMe testers!", function() {
+  ok( testMe("happy"),"testMe asserted successfully");
+  notEqual( testMe("unhappy"), true, "testMe failed as expected");
+});
