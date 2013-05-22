3fefbe92f71245accb30d107b9f191af23de4aac
Leif Nelson <leif@lwndev.com>
2013-05-21 21:28:52 -0500

    Checking in a fix for broken test in the new closure app setup

--- src/www/js/test/HomepageAppTests.js
+++ src/www/js/test/HomepageAppTests.js
@@ -1,5 +1,5 @@
 test( "testMe test!", function() {
   ok( application.testPublicFunction("happy"),"Function returned a value");
   equal( application.testPublicFunction("happy"),"happy", "Same value returned as passed to function");
-  notEqual( application.testPublicFunction("happy"),"happy", "A different value was not returned by the function");
+  notEqual( application.testPublicFunction("unhappy"),"happy", "A different value was not returned by the function");
 });
